5b802256f74e68c3e91ce1df4b6db565e590b63b
Armin <armin.meth@gmail.com>
2017-07-03 23:04:18 +0200

    Ancestor method overrides, handling no results

--- tdwreport.py
+++ tdwreport.py
@@ -1,29 +1,31 @@
 #! /usr/bin/env python
-import sys
 import argparse
+import requests
 from weather import Weather
+from weather import models
 
-class WeatherReport:
+class WeatherReport(Weather):
     def __init__(self, location):
+        Weather.__init__(self)
         self.get_report(location)   
 
     def get_report(self, location): 
         self.show_location(location)
-        [self.show_weather(day) for day in self.get_days(self.locate(location))]
+        [self.show_weather(day) for day in self.get_days(self.lookup_by_location(location))]
 
-    def locate(self, location):
-        return Weather().lookup_by_location(location)
+    def lookup_by_location(self, location):
+        url = "%s?q=select* from weather.forecast " \
+              "where woeid in (select woeid from geo.places(1) where text='%s') and u='c' &format=json" % (self.URL, location)
+        results = self._call(url)
+        return results
 
     def get_days(self, place):
         days = []
         [self.get_day(days, item['date'], item['low'], item['high'], item['text']) for item in place.forecast()]
         return days
 
-    def get_day(self, days, date, low, high, text): 
-        days.append([date, self.get_celsius(low), self.get_celsius(high), text])
-
-    def get_celsius(self, temperature):
-        return (int(temperature) - 32) * 5.0/9.0
+    def get_day(self, days, date, low, high, text):
+        days.append([date, int(low), int(high), text])
 
     def show_weather(self, day):
         from_to = "%d-%d" % (day[1], day[2])
@@ -35,9 +37,19 @@ class WeatherReport:
         print feedback.center(50)
         print "-" * 50
 
+    def _call(self, url):
+        results = requests.get(url).json()
+        if int(results['query']['count']) > 0:
+            wo = models.weather_obj.WeatherObject(results['query']['results']['channel'])
+            return wo
+        else:
+            print 'No results found.'
+            quit()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('tdwreport')
     parser.add_argument('-l', '--location', default="sarajevo", help='get forecast for this location')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.2')
     args = parser.parse_args()
     report = WeatherReport(args.location)
